abe83f2d672c78f901714116c402d6946d296c20
James Hyatt <james.hyatt@outlook.com>
2025-01-08 17:40:29 +0000

    Update page layout to enhance responsiveness and visual presentation
    - Modified the fixed overlay in page.tsx to be hidden on mobile and visible on larger screens, improving the user interface.
    - Ensured the layout maintains responsiveness while displaying additional content effectively.
    - Continued to refine the overall structure and user experience in line with previous enhancements.

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function Page() {
       <div className="p-2 laptop:p-4 flex flex-col laptop:gap-y-8 gap-y-4">
         <SliceZone slices={page.data.slices} components={components} />
       </div>
-      <div className="fixed w-full h-full flex z-50 inset-0 pointer-events-none p-8">
+      <div className="fixed w-full h-full z-50 inset-0 pointer-events-none p-8 hidden laptop:flex">
         <div className="w-1/2 h-full flex flex-col justify-center items-start">
           <div>Bloom [ 1/12 ]</div>
         </div>
