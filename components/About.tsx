8b799d9aa3ba3a489a97b4c40710de0e2dbab43f
James Hyatt <james.hyatt@outlook.com>
2025-01-24 12:58:06 +0000

    Add About component with animation and state management
    - Introduced a new atom, showAboutAtom, to manage the visibility of the About component.
    - Updated the About component to utilize framer-motion for animated transitions when displaying user information.
    - Enhanced the Info component to trigger the display of the About component through a click event.
    - These changes improve user interaction and provide a smoother experience when accessing information about the designer.

--- components/About.tsx
+++ components/About.tsx
@@ -1,30 +1,54 @@
+'use client'
+
+import { useAtom } from 'jotai'
+import { showAboutAtom } from '../atoms/about' // You'll need to create this file
+import { motion, AnimatePresence } from 'framer-motion' // For animation
+
 export function About() {
+  const [showAbout, setShowAbout] = useAtom(showAboutAtom)
+
   return (
-    <div className="fixed w-full h-full z-50 inset-0 pointer-events-none p-2 bg-white hidden">
-      <div className="w-1/2 h-full flex flex-col justify-center items-start">
-        <div className="text-sm">
-          Designer & Photographer, MCR.{' '}
-          <a href="mailto:hello@danielreed.photography">
-            hello@danielreed.photography
-          </a>
-          ,{' '}
-          <a href="https://www.instagram.com/danielreed.photography">
-            Instagram
-          </a>
-          .
-        </div>
-      </div>
-      <div className="flex-1 h-full flex flex-col justify-center items-end">
-        <div className="bg-blue-500 aspect-[394/524] w-[394px]"></div>
-      </div>
-      <div className="w-1/2 h-full flex flex-col justify-center items-end">
-        <div className="flex gap-x-4">
-          <div>Daniel Reed</div>
-          <a className=" pointer-events-auto cursor-pointer text-black/50">
-            About
-          </a>
-        </div>
-      </div>
-    </div>
+    <AnimatePresence>
+      {showAbout && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          className="fixed w-full h-full z-50 inset-0 p-2 bg-white flex cursor-pointer"
+        >
+          <div className="w-1/2 h-full flex flex-col justify-center items-start">
+            <div className="text-sm">
+              Designer & Photographer, MCR.{' '}
+              <a
+                href="mailto:hello@danielreed.photography"
+                className="hover:underline relative z-[60]"
+              >
+                hello@danielreed.photography
+              </a>
+              ,{' '}
+              <a
+                href="https://www.instagram.com/danielreed.photography"
+                className="hover:underline relative z-[60]"
+              >
+                Instagram
+              </a>
+              .
+            </div>
+          </div>
+          <div className="flex-1 h-full flex flex-col justify-center items-end">
+            <div className="bg-blue-500 aspect-[394/524] w-[394px]"></div>
+          </div>
+          <div className="w-1/2 h-full flex flex-col justify-center items-end">
+            <div className="flex gap-x-4">
+              <div>Daniel Reed</div>
+              <a className=" pointer-events-auto cursor-pointer text-black/50">
+                About
+              </a>
+            </div>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
--- components/Info.tsx
+++ components/Info.tsx
@@ -1,6 +1,11 @@
+'use client'
+
+import { showAboutAtom } from '@/atoms/about'
 import { BloomText } from '@/components/BloomText'
+import { useAtom } from 'jotai'
 
 export function Info() {
+  const [, setShowAbout] = useAtom(showAboutAtom)
   return (
     <div className="fixed w-full h-full z-50 inset-0 pointer-events-none p-2 hidden laptop:flex">
       <div className="w-1/2 h-full flex flex-col justify-center items-start">
@@ -9,7 +14,10 @@ export function Info() {
       <div className="w-1/2 h-full flex flex-col justify-center items-end">
         <div className="flex gap-x-4">
           <div>Daniel Reed</div>
-          <a className=" pointer-events-auto cursor-pointer text-black/50">
+          <a
+            className=" pointer-events-auto cursor-pointer text-black/50"
+            onClick={() => setShowAbout(true)}
+          >
             About
           </a>
         </div>
