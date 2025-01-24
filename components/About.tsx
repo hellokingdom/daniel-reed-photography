a10cd9d47f8cf30da3a9717f535bfbc8bcfa1fce
James Hyatt <james.hyatt@outlook.com>
2025-01-24 13:03:04 +0000

    Add image to About component and enhance interactivity

--- components/About.tsx
+++ components/About.tsx
@@ -3,6 +3,8 @@
 import { useAtom } from 'jotai'
 import { showAboutAtom } from '../atoms/about' // You'll need to create this file
 import { motion, AnimatePresence } from 'framer-motion' // For animation
+import Image from 'next/image'
+import img from '@/assets/daniel-reed.jpg'
 
 export function About() {
   const [showAbout, setShowAbout] = useAtom(showAboutAtom)
@@ -16,6 +18,7 @@ export function About() {
           exit={{ opacity: 0 }}
           transition={{ duration: 0.3 }}
           className="fixed w-full h-full z-50 inset-0 p-2 bg-white flex cursor-pointer"
+          onClick={() => setShowAbout(false)}
         >
           <div className="w-1/2 h-full flex flex-col justify-center items-start">
             <div className="text-sm">
@@ -23,6 +26,7 @@ export function About() {
               <a
                 href="mailto:hello@danielreed.photography"
                 className="hover:underline relative z-[60]"
+                onClick={(e) => e.stopPropagation()}
               >
                 hello@danielreed.photography
               </a>
@@ -30,6 +34,7 @@ export function About() {
               <a
                 href="https://www.instagram.com/danielreed.photography"
                 className="hover:underline relative z-[60]"
+                onClick={(e) => e.stopPropagation()}
               >
                 Instagram
               </a>
@@ -37,7 +42,9 @@ export function About() {
             </div>
           </div>
           <div className="flex-1 h-full flex flex-col justify-center items-end">
-            <div className="bg-blue-500 aspect-[394/524] w-[394px]"></div>
+            <div className="bg-blue-500 aspect-[394/524] w-[394px] relative">
+              <Image src={img} fill alt="Daniel Reed" placeholder="blur" />
+            </div>
           </div>
           <div className="w-1/2 h-full flex flex-col justify-center items-end">
             <div className="flex gap-x-4">
